1d9fb5fd3ee757bfac6440c1a8d427297c6795a4
EverettCooley <Cooleyeverett4@gmail.com>
2022-07-26 08:39:30 -0700

    Initial Commit

---
+++ Travel-Spots/Travel_SpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_SpotsApp.swift
+//  Travel-Spots
+//
+//  Created by v on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Travel_SpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
